ac34413580fdff3c8db7bf1b4d6d73d785e45b5f
lorry <ly3124675luces@163.com>
2023-10-13 17:42:37 +0800

    init client frame.

---
+++ ClientApp/app/src/main/java/com/zrh/clientapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.zrh.clientapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //获得控件
+        WebView webView = findViewById(R.id.webView);
+        //访问网页
+        webView.loadUrl("http://114.132.232.157");
+        //开启JavaScript
+        webView.getSettings().setJavaScriptEnabled(true);
+        //系统默认会通过手机浏览器打开网页，为了能够直接通过WebView显示网页，则必须设置
+        webView.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url == null) return false;
+
+                try{
+                    if(!url.startsWith("http://") && !url.startsWith("https://")){
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        startActivity(intent);
+                        return true;
+                    }
+                }catch (Exception e){//防止crash (如果手机上没有安装处理某个scheme开头的url的APP, 会导致crash)
+                    return true;//没有安装该app时，返回true，表示拦截自定义链接，但不跳转，避免弹出上面的错误页面
+                }
+
+                // TODO Auto-generated method stub
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+    }
+}
