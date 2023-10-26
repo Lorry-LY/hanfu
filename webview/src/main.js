6286706123f234fc53753c40aae6c13ac6a84705
Yu Luo <ly3124675luces@163.com>
2023-10-27 00:31:23 +0800

    将前端框架从Vue2升级到Vue3

--- webview/src/main.js
+++ webview/src/main.js
@@ -1,15 +1,9 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import { createApp } from 'vue'
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-Vue.config.productionTip = false
+const app = createApp(App)
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+app.use(ElementPlus)
+app.mount('#app')
---
+++ webview/src/utils/http/http.js
@@ -0,0 +1,34 @@
+import {baseUrl} from '../../config/env'
+import axios from 'axios'
+
+export default async (url = '', data = {}, type = 'GET', method = 'fetch') => {
+    type = type.toUpperCase();
+    url = baseUrl + url;
+    console.log(type, url, data)
+    if (type === 'GET') {
+        await axios.get(url, {params: data}).then(res => {
+            if (res.data.message.success === true) {
+                console.log(res.data.message)
+                data = res.data.message
+            } else {
+                throw new Error(res.data.message.message)
+            }
+        }).catch(e => {
+            throw new Error(e)
+        })
+        return data;
+    }
+    if (type === 'POST') {
+        await axios.post(url, data).then(res => {
+            if (res.data.message.success === true) {
+                console.log(res.data.message)
+                data = res.data.message
+            } else {
+                throw new Error(res.data.message.message)
+            }
+        }).catch(e => {
+            throw new Error(e)
+        })
+        return data;
+    }
+}
